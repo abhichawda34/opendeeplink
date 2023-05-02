a8af3f7fae1672c9eafd3c615c6e8b5e0f1a5660
Abhishek Chawda <abhishek.mali@myntra.com>
2023-05-03 02:46:37 +0530

    simulating anchor tag

--- src/App.js
+++ src/App.js
@@ -7,24 +7,19 @@ function App() {
   const simulateClick = (e) => {
     e.click();
   }
-  const onButtonClick = () => {
-    window.location.href = "https://www.myntra.com/giftcard";
-  }
+  // const onButtonClick = () => {
+  //   window.location.href = "https://www.myntra.com/giftcard";
+  // }
 
   return (
 		<div className="App">
 			<header className="App-header">
 				<img src={logo} className="App-logo" alt="logo" />
-				<div ref={simulateClick} onClick={onButtonClick}>
+				{/* <div ref={simulateClick} onClick={onButtonClick}>
 					Click me
-				</div>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
+				</div> */}
+				<a ref={simulateClick} href="https://www.myntra.com/cbc/store">
+					Click me
 				</a>
 			</header>
 		</div>
