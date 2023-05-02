9df9a785295cd5ababeb141600cf59002f92f07f
Abhishek Chawda <abhishek.mali@myntra.com>
2023-05-03 02:37:56 +0530

    adding link

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const simulateClick = (e) => {
+    e.click();
+  }
+  const onButtonClick = () => {
+    window.location.href = "https://www.myntra.com/giftcard";
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload...
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<div ref={simulateClick} onClick={onButtonClick}>
+					Click me
+				</div>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+			</header>
+		</div>
+	);
 }
 
 export default App;
